cdc8d3026a8cb14467890178d51011d5b476b4e3
joechip125 <mattias.lonnqvist@student.forsbergsskola.se>
2022-09-06 09:10:56 +0200

    update

--- MyProject/Source/MyProject/TP_ThirdPerson/TP_ThirdPersonCharacter.cpp
+++ MyProject/Source/MyProject/TP_ThirdPerson/TP_ThirdPersonCharacter.cpp
@@ -13,6 +13,7 @@
 
 ATP_ThirdPersonCharacter::ATP_ThirdPersonCharacter()
 {
+	Perception = CreateDefaultSubobject<UAIPerceptionStimuliSourceComponent>("Perception");
 	// Set size for collision capsule
 	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
 
