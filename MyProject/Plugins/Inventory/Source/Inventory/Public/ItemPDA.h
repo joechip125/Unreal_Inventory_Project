3cb13b4e32490e57434c9a262a052dffbc1cda51
joechip125 <mattias.lonnqvist@student.forsbergsskola.se>
2022-09-12 12:35:58 +0200

    started moving inventory to c++

---
+++ MyProject/Plugins/Inventory/Source/Inventory/Public/ItemPDA.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "ItemPDA.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INVENTORY_API UItemPDA : public UDataAsset
+{
+	GENERATED_BODY()
+};
