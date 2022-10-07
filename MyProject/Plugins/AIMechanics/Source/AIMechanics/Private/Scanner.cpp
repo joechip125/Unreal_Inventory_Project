a733381ff7f4119268c6b177561ee30a86d94fcf
joechip125 <mattias.lonnqvist@student.forsbergsskola.se>
2022-10-07 17:23:50 +0200

    created scan script

---
+++ MyProject/Plugins/AIMechanics/Source/AIMechanics/Private/Scanner.cpp
@@ -0,0 +1,58 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Scanner.h"
+
+#include "../../../../Developer/RiderLink/Source/RD/thirdparty/spdlog/include/spdlog/fmt/bundled/format.h"
+
+
+// Sets default values
+AScanner::AScanner()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	RenderComponent = CreateDefaultSubobject<UAreaRenderingComponent>(TEXT("RenderComp"));
+	RenderComponent->SetupAttachment(GetRootComponent());
+}
+
+// Called when the game starts or when spawned
+void AScanner::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AScanner::CircleScan(FVector Center, float Radius, int numberScans)
+{
+	RenderComponent->Lines.Empty(numberScans);
+	float degree = 0;
+	numberScans = FMath::Clamp(numberScans, 1, 360);
+	const int increment = 360 / numberScans;
+	
+	for(int i = 0; i <  numberScans; i++)
+	{
+		float theSin = FMath::Sin(FMath::DegreesToRadians(degree)) * Radius;
+		float theCos = FMath::Cos(FMath::DegreesToRadians(degree)) * Radius;
+		auto pos = Center + FVector(theCos, theSin,0);
+		DoATrace(pos, Center);
+		
+		auto line = FEditorVisLine(pos, Center, FColor::Green);
+		RenderComponent->Lines.Add(line);
+
+		degree += increment;
+	}
+}
+
+void AScanner::DoATrace(FVector Start, FVector End)
+{
+	auto HitResult = FHitResult();
+	auto Query = FCollisionObjectQueryParams(ECollisionChannel::ECC_WorldStatic);
+	GetWorld()->LineTraceSingleByObjectType(HitResult, Start, End, Query);
+}
+
+// Called every frame
+void AScanner::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ MyProject/Plugins/AIMechanics/Source/AIMechanics/Public/Scanner.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AreaRenderingComponent.h"
+#include "GameFramework/Actor.h"
+#include "Scanner.generated.h"
+
+UCLASS()
+class AIMECHANICS_API AScanner : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AScanner();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintCallable)
+	void CircleScan(FVector Center, float Radius, int numberScans);
+	
+	void DoATrace(FVector Start, FVector End);
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	UAreaRenderingComponent* RenderComponent;
+};
