1322813967888647e814b966a90fdd7abe32250b
joechip125 <mattias.lonnqvist@student.forsbergsskola.se>
2022-10-19 12:40:40 +0200

    update

---
+++ MyProject/Source/MyProject/Private/AttributeBase.cpp
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AttributeBase.h"
+
+#include "Net/UnrealNetwork.h"
+
+
+void UAttributeBase::OnRep_Health(const FGameplayAttributeData& OldHealth)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAttributeBase, Health, OldHealth)
+}
+
+void UAttributeBase::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UAttributeBase, Health, COND_None, REPNOTIFY_Always)
+}
+
+void UAttributeBase::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	Super::PreAttributeChange(Attribute, NewValue);
+
+	if(Attribute == GetHealthAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0, 100);
+	}
+}
---
+++ MyProject/Source/MyProject/Public/AttributeBase.h
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "AttributeSet.h"
+#include "UObject/Object.h"
+#include "AttributeBase.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
+
+UCLASS()
+class MYPROJECT_API UAttributeBase : public UAttributeSet
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_Health)
+
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UAttributeBase, Health);
+
+	UFUNCTION()
+	virtual void OnRep_Health(const  FGameplayAttributeData& OldHealth);
+
+	virtual void GetLifetimeReplicatedProps( TArray< class FLifetimeProperty > & OutLifetimeProps ) const override;
+
+public:
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+};
